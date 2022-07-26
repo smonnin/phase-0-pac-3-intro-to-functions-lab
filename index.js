7a0746b1878e125587e60072c84deaf7c67538e8
shawnmonnin03@gmail.com <shawnmonnin03@gmail.com>
2022-07-26 13:40:18 -0700

    commit tree 081b593470f4649e12cbb94ee993b56a949817ef

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    // todo
+  }
+function shout(string) {
+    return string.toUpperCase();
+  }
+//Whisper function below
+  function whisper(string) {
+    // todo
+  }
+function whisper(string) {
+    return string.toLowerCase();
+  }
+//Shout function below
+function logShout(string) {
+    console.log("HELLO")
+}
+//Whisper function below
+function logWhisper(string) {
+    console.log("hello")
+
+    console.log(string)
+}
+//Headphone function below, use 1 function
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string) 
+    console.log("I can\'t hear you!");
+}
